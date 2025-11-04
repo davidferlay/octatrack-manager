c287ef68d0075af2b6ff9347631fec62de69102f
David Ferlay <dferlay@skilld.cloud>
2025-11-04 22:37:48 +0100

    Added ability to browse custom locations

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,6 @@
 mod device_detection;
 
-use device_detection::{discover_devices, OctatrackLocation};
+use device_detection::{discover_devices, scan_directory, OctatrackLocation};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -13,11 +13,17 @@ fn scan_devices() -> Vec<OctatrackLocation> {
     discover_devices()
 }
 
+#[tauri::command]
+fn scan_custom_directory(path: String) -> Vec<OctatrackLocation> {
+    scan_directory(&path)
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, scan_devices])
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![greet, scan_devices, scan_custom_directory])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
